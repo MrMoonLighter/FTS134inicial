b12c3f6a8b1e173c8c61e34940eab0e916280f84
Anrry <anrry.petrin@gmail.com>
2022-06-04 21:05:49 -0300

    Criação de teste de unidade com leitura de uma lista de valores

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,8 +1,9 @@
+import pytest
+
 from main import somar, dividir
 
 
 def teste_somar():
-
     # 1 - Configura
     numero_a = 8
     numero_b = 7
@@ -46,6 +47,27 @@ def teste_dividir_negativo():
     # 3 - Valida
     assert resultado_obtido == resultado_esperado
 
+
+# lista para o uso como massa de teste
+lista_de_valores = [
+    (8, 7, 15),
+    (20, 30, 50),
+    (25, 0, 25),
+    (-5, 12, 7),
+    (6, -3, 3)
+]
+
+
+@pytest.mark.parametrize('numero_a, numero_b, resultado_esperado', lista_de_valores)
+def teste_somar_leitura_de_lista(numero_a, numero_b, resultado_esperado):
+    # 1 - Configura
+    # utilizamos a lista como massa de teste
+
+    # 2 - Executa
+    resultado_obtido = somar(numero_a, numero_b)
+
+    # 3 - Valida
+    assert resultado_obtido == resultado_esperado
 
 # TDD = Test Driven Development
 #       Desenvilvimento Direcionado por Teste
